171a5d5263b68f07e64300ef20a01cde2e925af5
l.maines <lmmaines28@gmail.com>
2024-08-22 22:52:29 -0400

    wrote inquirer prompts for logo input

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const inquirer = require('inquirer');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for your logo text:',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (color keyword or hex):',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+        type: 'input',
+        name: 'shareColor',
+        message: 'Enter the shape color (color keyword or hex):',
+    }
+]).then(answers => {
+    let shape; 
+    if (answers.shape === 'Circle') {
+        shape = new Circle();
+    } else if (answers.shape === 'Triangle') {
+        shape = new Triangle();
+    } else if (answers.shape === 'Square') {
+        shape = new Square();
+    }
+    shape.setColor(answers.shapeColor);
+    
+})
