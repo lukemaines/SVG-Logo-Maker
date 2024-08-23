8df653023fe9b46ca0a2721e5427230f60918b89
l.maines <lmmaines28@gmail.com>
2024-08-22 23:40:47 -0400

    ran tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
-
+// user prompts for logo design
 inquirer.prompt([
     {
         type: 'input',
@@ -20,9 +21,10 @@ inquirer.prompt([
     },
     {
         type: 'input',
-        name: 'shareColor',
+        name: 'shapeColor',
         message: 'Enter the shape color (color keyword or hex):',
     }
+    // taking in user answers to create the correct shape
 ]).then(answers => {
     let shape; 
     if (answers.shape === 'Circle') {
@@ -33,5 +35,14 @@ inquirer.prompt([
         shape = new Square();
     }
     shape.setColor(answers.shapeColor);
-    
-})
+
+    const svgContent = `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+   ${shape.render()}
+   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+</svg>`;
+
+fs.writeFileSync('logo.svg', svgContent);
+console.log('Generated logo.svg');
+
+});
